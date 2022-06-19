9ae2dd0e4a556ba1b3badab9bff66cb176e1b6c4
NanaAb-116 <nanaabboateng@gmail.com>
2022-06-19 09:00:34 +0000

    add shake and images after shake

--- src/components/Game.js
+++ src/components/Game.js
@@ -16,7 +16,7 @@ function Game() {
   const [playerChoice, setPlayerChoice] = useState(null);
   const [computerChoice, setComputerChoice] = useState(null);
   const [playerImage, setPlayerImage] = useState(rock);
-  const [computerImage, setComputerImage] = useState(paper);
+  const [computerImage, setComputerImage] = useState(rock);
   const [shakeAnimation, setShakeAnimation] = useState(false);
 
   // Switch screens
@@ -25,24 +25,32 @@ function Game() {
     setFadeIn(false);
   };
 
+  const panimate = { animation: 'shakePlayer 2s ease' };
+  const canimate = { animation: 'shakeComputer 2s ease' };
+  const noAnimate = { animation: '' };
+
   const animationHandler = () => {
-    setShakeAnimation(false);
     setShakeAnimation(true);
-    setShakeAnimation('shakePlayer 2s ease');
+  };
+
+  const stopAnimation = () => {
+    setShakeAnimation(false);
   };
 
   const playerClickHandler = (hand) => {
+    stopAnimation();
     setPlayerChoice(hand);
     getComputersChoice();
+  };
+
+  useEffect(() => {
     animationHandler();
     setTimeout(() => {
       // here is where we call compare hands
       compareHands();
-      //   // update images
-      //   setPlayerImage(`../assets/${hand}.png`);
-      //   setComputerImage(computerChoice);
-    }, 10);
-  };
+    }, 2000);
+  }, [computerChoice, playerChoice]);
+
   const getComputersChoice = () => {
     //computer choice
     const computerNumber = Math.floor(Math.random() * computerOptions.length);
@@ -53,30 +61,36 @@ function Game() {
   const compareHands = () => {
     console.log(playerChoice);
     console.log(computerChoice);
-    if (playerChoice === computerChoice) {
-      setResult("It's a tie");
-      return;
-    }
-    //checck for rock
     if (playerChoice === 'rock') {
+      setPlayerImage(rock);
       if (computerChoice === 'scissors') {
+        setComputerImage(scissors);
         setResult('Player Wins');
         setPlayerScore(playerScore + 1);
-
         return;
+      } else if (computerChoice === 'rock') {
+        setComputerImage(rock);
+        setResult("It's a tie");
       } else {
         setResult('Computer Wins');
+        setComputerImage(paper);
         setComputerScore(computerScore + 1);
         return;
       }
     }
     // check for paper
     if (playerChoice === 'paper') {
+      setPlayerImage(paper);
       if (computerChoice === 'scissors') {
+        setComputerImage(scissors);
         setResult('Computer Wins');
         setComputerScore(computerScore + 1);
         return;
+      } else if (computerChoice === 'paper') {
+        setComputerImage(paper);
+        setResult("It's a tie");
       } else {
+        setComputerImage(rock);
         setResult('Player Wins');
         setPlayerScore(playerScore + 1);
         return;
@@ -84,10 +98,16 @@ function Game() {
     }
     // check for scissors
     if (playerChoice === 'scissors') {
+      setPlayerImage(scissors);
       if (computerChoice === 'rock') {
+        setComputerImage(rock);
         setResult('Computer Wins');
         setComputerScore(computerScore + 1);
+      } else if (computerChoice === 'scissors') {
+        setComputerImage(scissors);
+        setResult("It's a tie");
       } else {
+        setComputerImage(paper);
         setResult('Player Wins');
         setPlayerScore(playerScore + 1);
         return;
@@ -120,8 +140,17 @@ function Game() {
               >
                 <h2 className='winner my-5'>{result}</h2>
                 <div className='hands'>
-                  <img className='player-hand' src={playerImage} alt='' />
-                  <img className='computer-hand' src={computerImage} />
+                  <img
+                    className='player-hand'
+                    src={playerImage}
+                    alt=''
+                    style={shakeAnimation ? panimate : noAnimate}
+                  />
+                  <img
+                    className='computer-hand'
+                    src={computerImage}
+                    style={shakeAnimation ? canimate : noAnimate}
+                  />
                 </div>
                 <div className='options'>
                   <button
