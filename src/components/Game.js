af1f0d1e1d534083aef9a1e2349d5ef2abb009a5
NanaAb-116 <nanaabboateng@gmail.com>
2022-06-19 01:45:32 +0000

    initial commit

---
+++ src/components/Game.js
@@ -0,0 +1,161 @@
+import React, { useState, useEffect } from 'react';
+
+import rock from '../assets/rock.png';
+import paper from '../assets/paper.png';
+import scissors from '../assets/scissors.png';
+
+function Game() {
+  // computer options
+  const computerOptions = ['rock', 'paper', 'scissors'];
+
+  const [fadeOut, setFadeOut] = useState(true);
+  const [fadeIn, setFadeIn] = useState(true);
+  const [playerScore, setPlayerScore] = useState(0);
+  const [computerScore, setComputerScore] = useState(0);
+  const [result, setResult] = useState('Choose an option');
+  const [playerChoice, setPlayerChoice] = useState(null);
+  const [computerChoice, setComputerChoice] = useState(null);
+  const [playerImage, setPlayerImage] = useState(rock);
+  const [computerImage, setComputerImage] = useState(paper);
+  const [shakeAnimation, setShakeAnimation] = useState(false);
+
+  // Switch screens
+  const startGameHundler = () => {
+    setFadeOut(false);
+    setFadeIn(false);
+  };
+
+  const animationHandler = () => {
+    setShakeAnimation(false);
+    setShakeAnimation(true);
+    setShakeAnimation('shakePlayer 2s ease');
+  };
+
+  const playerClickHandler = (hand) => {
+    setPlayerChoice(hand);
+    getComputersChoice();
+    animationHandler();
+    setTimeout(() => {
+      // here is where we call compare hands
+      compareHands();
+      //   // update images
+      //   setPlayerImage(`../assets/${hand}.png`);
+      //   setComputerImage(computerChoice);
+    }, 10);
+  };
+  const getComputersChoice = () => {
+    //computer choice
+    const computerNumber = Math.floor(Math.random() * computerOptions.length);
+    const randomComputerChoice = computerOptions[computerNumber];
+
+    setComputerChoice(randomComputerChoice);
+  };
+  const compareHands = () => {
+    console.log(playerChoice);
+    console.log(computerChoice);
+    if (playerChoice === computerChoice) {
+      setResult("It's a tie");
+      return;
+    }
+    //checck for rock
+    if (playerChoice === 'rock') {
+      if (computerChoice === 'scissors') {
+        setResult('Player Wins');
+        setPlayerScore(playerScore + 1);
+
+        return;
+      } else {
+        setResult('Computer Wins');
+        setComputerScore(computerScore + 1);
+        return;
+      }
+    }
+    // check for paper
+    if (playerChoice === 'paper') {
+      if (computerChoice === 'scissors') {
+        setResult('Computer Wins');
+        setComputerScore(computerScore + 1);
+        return;
+      } else {
+        setResult('Player Wins');
+        setPlayerScore(playerScore + 1);
+        return;
+      }
+    }
+    // check for scissors
+    if (playerChoice === 'scissors') {
+      if (computerChoice === 'rock') {
+        setResult('Computer Wins');
+        setComputerScore(computerScore + 1);
+      } else {
+        setResult('Player Wins');
+        setPlayerScore(playerScore + 1);
+        return;
+      }
+    }
+  };
+
+  return (
+    <div>
+      <section className='game'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-12 text-center'>
+              <div className='score'>
+                <div className='player-score'>
+                  <h2>Player</h2>
+                  <p>{playerScore}</p>
+                </div>
+                <div className='computer-score'>
+                  <h2>Computer</h2>
+                  <p>{computerScore}</p>
+                </div>
+              </div>
+              <div className={fadeOut ? 'intro' : 'fadeOut intro'}>
+                <h1>Rock Paper Scissors</h1>
+                <button onClick={startGameHundler}>Let's Play</button>
+              </div>
+              <div
+                className={fadeIn ? 'match fadeOut' : 'match fadeOut fadeIn'}
+              >
+                <h2 className='winner my-5'>{result}</h2>
+                <div className='hands'>
+                  <img className='player-hand' src={playerImage} alt='' />
+                  <img className='computer-hand' src={computerImage} />
+                </div>
+                <div className='options'>
+                  <button
+                    className='rock'
+                    onClick={() => {
+                      playerClickHandler('rock');
+                    }}
+                  >
+                    rock
+                  </button>
+                  <button
+                    className='rock'
+                    onClick={() => {
+                      playerClickHandler('paper');
+                    }}
+                  >
+                    paper
+                  </button>
+                  <button
+                    className='rock'
+                    onClick={() => {
+                      playerClickHandler('scissors');
+                    }}
+                  >
+                    scissors
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Game;
